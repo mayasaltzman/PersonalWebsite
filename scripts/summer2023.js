1dc8c94fcffd6d877aa643210fe1457064c9320a
Maya Saltzman <mayasaltzman3@gmail.com>
2023-08-24 14:46:24 -0400

    buttons for work term report working correctly

--- scripts/summer2023.js
+++ scripts/summer2023.js
@@ -1,34 +1,54 @@
 $(document).ready(
     function () {
+        //showing intro text when intro button is clicked
         $("#intro").click(
             function (e) {
-                $("#i1").text("While pursuing my degree in Computer Science I have developed an interest in databases and database management. During my Summer 2023 term, I worked as a Data Management Assistant for Kenna where I got to explore my interest in databases and expand my knowledge on quality assurance (QA) and software testing. This position combined my interest in tech with love for the outdoors because the software I worked on was used to help those working in agriculture.");
-                $("#i2").text("In this report, I will describe my employer and the organization, the position I worked in, what I was required to do, and I will finish off by going over my goals to outline what I accomplished in my first work term. ");
+                $('#introText').show();
+                $('#aboutText').hide();
+                $('#descriptionText').hide();
+                $('#goalsText').hide();
+                $('#caText').hide();
             }
         );
+        //showing about text when button is clicked
         $("#about").click(
             function (e) {
-                $("#introText").text("");
+                $('#introText').hide();
+                $('#aboutText').show();
+                $('#descriptionText').hide();
+                $('#goalsText').hide();
+                $('#caText').hide();
+                $("#h2a").text("About Employer");
                 $("#a1").text("Kenna is a marketing solutions provider specializing in customer experience management. Founded in 1985 as AppliCan Marketing and rebranded as Kenna 2002. For over three decades Kenna has provided their clients data driven solutions and software with an emphasis on customer first thinking. ");
                 $("#a2").text("Today Kenna works with BASF to create software for clients. For the past two decades Kenna has helped BASF build loyal customer relationships which have increased market success. This is done by gaining the trust of growers through their experience with the brand.");
                 $("#a3").text("The current CEO of Kenna is Jeffery Bowles and Kenna’s team continues to grow alongside BASF to better provide them solutions.");
 
             }
         );
-
+        //showing description text when button is clicked
         $("#description").click(
             function (e) {
-                $("#introText").text("");
+                $('#introText').hide();
+                $('#aboutText').hide();
+                $('#descriptionText').show();
+                $('#goalsText').hide();
+                $('#caText').hide();
+                $("#h2d").text("Job Description");
                 $("#d1").text("As a Data Management Assistant I worked for Data Solutions under the Data Management Team. My tasks differed from day to day but these were a few of my main responsibilities:");
                 $("#d2").text("Through the work term I performed QA and software testing when needed. I worked on 5 applications to ensure 99% accuracy. I used SQL, Excel, and IBM Notes to help me with the QA process. I learned about the software development life cycle and how an application goes from development to testing/staging to production.");
                 $("#d3").text("I was in charge of overseeing the process to set up employee profiles and territory updates. I managed Jira tickets coordinating responsibilities between developers and other Data Solutions team members. Then I performed overall QA on the whole process to make sure the employee/territory was set up correctly.");
                 $("#d4").text("I helped create a database dictionary for employee training. I wrote SQL queries to access data related to data points and calculations for specific applications. This taught me more about the contents of the applications I had been working on. The database dictionary will be a helpful tool for future Kenna employees and I am glad that I got to make an impact to help the company.");
             }
         );
-
+        //showing goals text when button is clicked
         $("#goals").click(
             function (e) {
-                $("#introText").text("");
+                $('#introText').hide();
+                $('#aboutText').hide();
+                $('#descriptionText').hide();
+                $('#goalsText').show();
+                $('#caText').hide();
+                $("#h2g").text("Goals");
                 $("#g1").text("1) Improve my written communication skills so that I can converse effectively in a professional manner.")
                 $("#g2").text("One thing I was nervous about going into my work term was my ability to communicate professionally. 	Not only did my written communication skills improve but my confidence in my ability to communicate has increased tremendously. I don't second guess myself when sending emails or Teams messages and I feel comfortable reaching out. I notice this confidence that I have learned continuing in my life outside of work. When sending emails related to school or extracurriculars I can do so with ease, and I have lost all the nervousness surrounding written communication that I had in the past. My supervisor even noted that one of my skills is my ability to communicate. This assured me that I was successful in achieving my goal.")
                 $("#g3").text("2) Improve my Excel skills.")
@@ -38,9 +58,15 @@ $(document).ready(
             }
         );
 
+        //showing conclusion and acknowledgements text when button is clicked
         $("#ca").click(
             function (e) {
-                $("#introText").text("");
+                $('#introText').hide();
+                $('#aboutText').hide();
+                $('#descriptionText').hide();
+                $('#goalsText').hide();
+                $('#caText').show();
+                $("#h2ca").text("Conclusion + Acknowledgments");
                 $("#ca1").text("I would like to thank my supervisor Daniel Kostevskii for supporting me throughout the work term. As well as everyone on the Data Management team for being so open and welcoming. All my team members were always there to provide me support and answer any questions. I would like to thank Kenna for fostering such a positive work environment. That wouldn’t be the same without all the amazing people that work there. One of the best things about working at Kenna was the company culture and I would like to thank all the people that planned in person social events despite the remote work environment. ")
                 $("#ca2").text("Working at Kenna was an amazing experience. I was able to learn a lot about the software development process and view software in a different light. I truly understand the importance of the user's experience thanks to all the QA and software testing I have done. I made so many amazing connections and I look forward to returning to Kenna for my next work term in the Fall!")
             }
